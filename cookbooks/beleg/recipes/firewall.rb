ce42fcba5dab5c7982f7195e05aa23374c0b46bd
Eric Brandt <ericbrandt@gmx.de>
2017-12-29 12:10:12 +0100

    firewall

--- cookbooks/beleg/recipes/firewall.rb
+++ cookbooks/beleg/recipes/firewall.rb
@@ -3,3 +3,32 @@
 # Recipe:: firewall
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
+
+
+
+firewall 'default'
+
+firewall_rule 'ssh' do
+	port 22
+	command :allow
+end
+
+
+firewall_rule 'http' do
+	port 80
+	protocol :tcp
+	command :allow
+end
+
+
+firewall_rule 'https' do
+	port 443
+	protocol :tcp
+	command :allow
+end
+
+
+firewall_rule 'remotedesk' do
+	port 3389
+	command :deny
+end
