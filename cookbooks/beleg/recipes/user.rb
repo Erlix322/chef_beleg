9e39970c49f6329c21999c849e564e323b3b5791
Eric Brandt <ericbrandt@gmx.de>
2018-01-02 10:22:14 +0100

    fixed user script

--- cookbooks/beleg/recipes/user.rb
+++ cookbooks/beleg/recipes/user.rb
@@ -6,15 +6,23 @@
 
 group 'users'
 
-directory_name = "/home/labornutzer"
+
+
+directory_name = "/home/labornutzer4"
 Dir.mkdir(directory_name) unless File.exists?(directory_name)
 
-user 'labornutzer' do
+
+
+user 'labornutzer4' do
 	action :create
 	group 'users'
-	system true
 	shell '/bin/bash'
 	password '$1$LTzdkMd2$5Mht956UnTjk1VFQhNu2Q0'
         manage_home true
-	home '/home/labornutzer'
+	home '/home/labornutzer4'
 end
+
+execute 'chown user directory' do
+	command 'chown labornutzer4:users /home/labornutzer4'
+end
+
